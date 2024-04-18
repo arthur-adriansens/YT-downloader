87493a69d27fee3b0a36c7982f78a2c392f9df30
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-04-18 21:39:13 +0200

    Small bug fix :)

--- download.py
+++ download.py
@@ -22,7 +22,11 @@ def Youtube_DL(link, mp3):
 		video.audio.write_audiofile(path.replace(".mp4", ".mp3"))
 		print("Converted to mp3!")
 
-		os.remove(path)
+		try:
+			os.remove(path)
+		except Exception:
+			print("De .mp3 is succesvol gemaakt, maar de .mp4/video is niet automatisch verwijderd. Dit is geen probleem en komt niet door mij, maar door Windows :). U kunt de .mp4/video gerust zelf verwijderen.")
+			pass
 
 Youtube_DL(arguments[1], arguments[2])
 # Youtube_DL("https://www.youtube.com/watch?v=YNVMdd6zMUE", "ja")
