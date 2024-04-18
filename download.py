8e43995a6b6dba7da16c324fc148646b23002920
Arthur <79015591+arthur-adriansens@users.noreply.github.com>
2024-04-18 12:36:11 +0200

    Remove mp4 file after mp3 conversion.
    Remove mp4 file after mp3 conversion.

--- download.py
+++ download.py
@@ -1,24 +1,28 @@
 from sys import argv as arguments
+import os
 
 from pytube import YouTube
 from moviepy.editor import *
 
+
 def Youtube_DL(link, mp3):
-    video = YouTube(link)
-    title = video.title
+	video = YouTube(link)
+	title = video.title
 
-    stream = video.streams.get_highest_resolution()
-    
-    print(f'Downloading "{title}"...')
-    path = stream.download("videos")
-    print(f'Downloaded "{title}"!')
+	stream = video.streams.get_highest_resolution()
 
-    if mp3 == "ja":
-        print("")
-        print("Converting to mp3...")
-        video = VideoFileClip(path)
-        video.audio.write_audiofile(path.replace(".mp4", ".mp3"))
-        print("Converted to mp3!")
+	print(f'Downloading "{title}"...')
+	path = stream.download("videos")
+	print(f'Downloaded "{title}"!')
+
+	if mp3 == "ja":
+		print("")
+		print("Converting to mp3...")
+		video = VideoFileClip(path)
+		video.audio.write_audiofile(path.replace(".mp4", ".mp3"))
+		print("Converted to mp3!")
+
+		os.remove(path)
 
 Youtube_DL(arguments[1], arguments[2])
-#Youtube_DL("https://www.youtube.com/watch?v=YNVMdd6zMUE", True)
+# Youtube_DL("https://www.youtube.com/watch?v=YNVMdd6zMUE", "ja")
